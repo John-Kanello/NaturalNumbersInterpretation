2769aab4ad10dc31b0a5dbf0a115f8ef2eb739d6
John-Kanello <ioanniskanello@gmail.com>
2025-03-15 12:37:09 +0200

    Add .idea to .gitignore

--- src/main/java/NaturalNumbersInterpretation.java
+++ src/main/java/NaturalNumbersInterpretation.java
@@ -5,6 +5,7 @@ import java.util.logging.Logger;
 public class NaturalNumbersInterpretation {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
     public static void main(String[] args) {
         while(true) {
             System.out.println("""
